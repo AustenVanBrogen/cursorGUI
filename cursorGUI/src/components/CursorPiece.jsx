1a6417ab94cc8a742f5dce7a22c1f3fb8ade2ca1
AustenVanBrogen <austenvanbrogen@yahoo.com>
2023-06-25 22:03:30 -0500

    There's now a function that detects when the icons are clicked, but it's running twice for some reason

--- cursorGUI/src/components/CursorPiece.jsx
+++ cursorGUI/src/components/CursorPiece.jsx
@@ -2,8 +2,12 @@ import './CursorPiece.css';
 import pic from '../assets/blackSquare.jpg';
 
 function CursorPiece({cursorName}){
+    function changeCursor(whichPiece){
+        console.log(whichPiece)
+    }
+
     return(
-        <div className={`cursorPiece ${cursorName}`}>
+        <div className={`cursorPiece ${cursorName}`} onClick={() => {changeCursor(changeCursor(cursorName.slice(-1)))}}>
             {/* <img src={pic}></img> */}
         </div>
     )
