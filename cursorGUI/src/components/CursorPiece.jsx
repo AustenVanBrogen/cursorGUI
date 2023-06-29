6e55fe21eac3c6c2f065ba27c29cb65ebfdf655b
AustenVanBrogen <austenvanbrogen@yahoo.com>
2023-06-29 18:24:43 -0500

    Clicking the buttons now changes a variable that is used to display a cursor when hovering over the display piece. Also fixed the bug where the changeCursor function was being called twice on the button click. The problem was that I was calling the function twice �

--- cursorGUI/src/components/CursorPiece.jsx
+++ cursorGUI/src/components/CursorPiece.jsx
@@ -1,14 +1,34 @@
 import './CursorPiece.css';
-import pic from '../assets/blackSquare.jpg';
+
+// function filterString(inputString){
+//     const firstIndex = 5;
+//     let filteredString;
+//     filteredString = inputString.substring(firstIndex, inputString.length);
+//     filteredString= filteredString.substring(0, filteredString.indexOf("\""))
+//     return filteredString;
+// }
 
 function CursorPiece({cursorName}){
     function changeCursor(whichPiece){
-        console.log(whichPiece)
+        //console.log(whichPiece)
+        //let pieceData = getComputedStyle(document.querySelector(whichPiece));
+        let cssifiedPiece = `.${whichPiece}`;
+        let nameOfCursor = getComputedStyle(document.querySelector(cssifiedPiece)).getPropertyValue('cursor');
+
+        // if(nameOfCursor.indexOf("url(") != -1)
+        // {
+        //     nameOfCursor = filterString(nameOfCursor);
+        // }
+
+        //document.querySelector(cssifiedPiece).style.setProperty('--curCursor', nameOfCursor);
+        document.querySelector(':root').style.setProperty('--curCursor', nameOfCursor);
+
+
+        //cursorVar.style.setProperty('--curCursor', nameOfCursor);
     }
 
     return(
-        <div className={`cursorPiece ${cursorName}`} onClick={() => {changeCursor(changeCursor(cursorName.slice(-1)))}}>
-            {/* <img src={pic}></img> */}
+        <div className={`cursorPiece ${cursorName}`} onClick={() => {changeCursor(cursorName)}}>
         </div>
     )
 }
